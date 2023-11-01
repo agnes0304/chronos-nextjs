9a070aea4ab9bfaef11df98beae49e6ac3cb8fde
agnes0304 <jiwoochoi0304@gmail.com>
2023-11-01 16:35:24 +0900

    added style effect

--- src/components/post/Actions.tsx
+++ src/components/post/Actions.tsx
@@ -5,19 +5,19 @@ import { faDownload, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Actions = () => {
   return (
-    <div className="flex justify-end gap-2">
+    <div className="flex justify-end gap-3">
       <button
         type="button"
-        className="h-[30px] border border-gray-400 rounded-full text-sm text-gray-400 flex justify-center items-center group p-1 px-2 hover:border-none hover:bg-indigo-300 hover:text-white hover:shadow-md hover:shadow-neutral-300 active:bg-indigo-400 active:shadow-inner"
+        className="h-[30px] border border-gray-400 rounded-full text-sm text-gray-400 flex justify-center items-center group p-1 px-2 hover:border-indigo-300 hover:bg-indigo-300 hover:text-white active:bg-indigo-400 active:shadow-inner transition-all duration-200 ease-in-out"
       >
-        <span className="hidden group-hover:inline mr-1">다운로드</span>
+        <span className="mr-1">다운로드</span>
         <FontAwesomeIcon icon={faDownload} />
       </button>
       <button
         type="button"
-        className="h-[30px] border border-gray-400 rounded-full text-sm text-gray-400 flex justify-center items-center group p-1 px-2 hover:border-none hover:bg-indigo-300 hover:text-white hover:shadow-md hover:shadow-neutral-300 active:bg-indigo-400 active:shadow-inner"
+        className="h-[30px] border border-gray-400 rounded-full text-sm text-gray-400 flex justify-center items-center group p-1 px-2 hover:border-indigo-300 hover:bg-indigo-300 hover:text-white active:bg-indigo-400 active:shadow-inner transition-all duration-200 ease-in-out"
       >
-        <span className="hidden group-hover:inline mr-1">자세히 보기</span>
+        <span className="mr-1">자세히 보기</span>
         <FontAwesomeIcon icon={faArrowRight} />
       </button>
     </div>
