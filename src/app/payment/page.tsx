9e213ef7418191ec08e02aac0167f96c03db6fdb
agnes0304 <jiwoochoi0304@gmail.com>
2023-11-23 13:06:12 +0900

    added complete payment page

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,9 +1,51 @@
-const Payment = () => {
-    return (
-        <div>
-            <h1>결제창</h1>
-        </div>
-    )
-}
+"use client";
+import { useEffect } from 'react';
+const userid = process.env.PAYAPP_USERID;
+const shopname = process.env.PAYAPP_SHOPNAME;
+const returnUrl = process.env.PAYAPP_RETURNURL;
 
-export default Payment
+declare var PayApp: any;
+
+const Payment = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '//lite.payapp.kr/public/api/payapp-lite.js';
+    script.async = true;
+
+    script.onload = () => {
+      PayApp.setDefault('userid', userid); // 필수
+      PayApp.setDefault('shopname', shopname); // 필수
+    };
+
+    document.body.appendChild(script);
+
+    function payappPay() {
+      PayApp.setParam('goodname', 'test good');
+      PayApp.setParam('price', '1000');
+    //   어떻게 price 전달할지.
+      PayApp.setParam('recvphone', '01000000000');
+      PayApp.setParam('returnurl', returnUrl);
+      PayApp.setParam('smsuse', 'n');
+      PayApp.setParam('redirectpay', '1');
+      PayApp.setParam('skip_cstpage', 'y');
+      PayApp.call();
+    }
+
+    const payButton = document.getElementById('payButton');
+    payButton?.addEventListener('click', payappPay);
+
+    return () => {
+      document.body.removeChild(script);
+      payButton?.removeEventListener('click', payappPay);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>결제창</h1>
+      <a href="#" id="payButton">결제하기</a>
+    </div>
+  );
+};
+
+export default Payment;
--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,7 +1,15 @@
+import axios from "axios"
+const baseurl = process.env.NEXT_PUBLIC_BASE_URL
+
+// 어떻게 구매한 파일의 다운로드 링크를 받아올지
+
 const Success = () => {
+
     return (
         <div>
-            <h1>Success</h1>
+            <h1>결제를 완료하셨다면 주문 시 입력한 핸드폰 번호를 입력 후 아래 다운로드 버튼을 눌러주세요!</h1>
+            <input type="text" placeholder="핸드폰 번호"></input>
+            <button>다운로드</button>
         </div>
     )
 }
