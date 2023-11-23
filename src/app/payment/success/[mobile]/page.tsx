f5dabcc258b6e715c4d84b20bbff2f0000acfb3c
agnes0304 <jiwoochoi0304@gmail.com>
2023-11-23 13:06:22 +0900

    added complete payment page

---
+++ src/app/payment/success/[mobile]/page.tsx
@@ -0,0 +1,12 @@
+const CompletePaymentData = () => {
+  return (
+    <div>
+      <h1>결제하신 상품입니다.</h1>
+        <h2>주의사항</h2>
+        <p>결제하신 상품은 1회에 한해 다운로드가 가능하며, 링크의 유효기간은 1일입니다.</p>
+        <p>본 페이지 링크의 유효기간 또한 1일입니다.</p>
+    </div>
+  );
+};
+
+export default CompletePaymentData;
