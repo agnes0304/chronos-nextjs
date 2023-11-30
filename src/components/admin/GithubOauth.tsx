0eddea9b6509722cdf1c6a839ac38f2fb0a7a1d1
agnes0304 <jiwoochoi0304@gmail.com>
2023-11-30 16:17:51 +0900

    oauth ongoing

--- src/components/admin/GithubOauth.tsx
+++ src/components/admin/GithubOauth.tsx
@@ -1,4 +1,5 @@
 "use client";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
@@ -16,11 +17,68 @@ const GithubOauth = () => {
     });
   }, []);
 
+  // db에 user의 id는 UID칼럼에, user의 email은 email칼럼에 저장하는 함수
+  async function saveUser(user: User) {
+    const { data, error } = await supabase
+      .from("users")
+      .insert([{ uid: user.id, email: user.email }]);
+    if (error) {
+      return { message: "user is not saved", error: true };
+    }
+    return { message: data, error: false };
+  }
+
+  async function saveAccessToken() {
+    const token = localStorage.getItem("sb-ezbyvglcocakzgsptqkw-auth-token");
+    if (token === null) {
+      return { message: "accessToken is null", error: true };
+    } else {
+      const accessToken = JSON.parse(token as string)["access_token"];
+      // const accessToken = JSON.parse(token as string)["current_session"]["access_token"];
+      localStorage.setItem("accessToken", accessToken);
+      return { message: "accessToken is saved", error: false };
+    }
+  }
+
+  async function sendTokenToServer() {
+    const token = localStorage.getItem("sb-ezbyvglcocakzgsptqkw-auth-token");
+    if (token === null) {
+      return { message: "refreshToken is null", error: true };
+    }
+
+    const refreshToken = JSON.parse(token as string)["refresh_token"];
+
+    try {
+      const response = await fetch(`${baseUrl}/set-refresh-token`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ refreshToken: refreshToken }),
+      });
+
+      if (response.ok) {
+        console.log("Token sent successfully");
+        const data = await response.text();
+        console.log(data);
+      } else {
+        console.error("Failed to send refreshToken", response.status);
+      }
+    } catch (error) {
+      console.error("Error sending refreshToken", error);
+    }
+  }
+
   async function checkUser() {
     const {
       data: { user },
     } = await supabase.auth.getUser();
     setUser(user);
+    if (user) {
+      await saveUser(user);
+      await saveAccessToken();
+      await sendTokenToServer();
+    }
   }
 
   async function signInWithGithub() {
@@ -32,7 +90,14 @@ const GithubOauth = () => {
 
   async function signOut() {
     await supabase.auth.signOut();
+    localStorage.removeItem("accessToken");
     setUser(null);
+    await fetch(`${baseUrl}/clear-refresh-token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
   if (user) {
@@ -46,7 +111,7 @@ const GithubOauth = () => {
           <FontAwesomeIcon icon={faGithub} className="mr-2" />
           Github 로그아웃
         </button>
-        {user && <p className="text-white">Signed in: {user.email}</p>}
+        {user && <p className="text-black">Signed in: {user.email}</p>}]
       </div>
     );
   }
