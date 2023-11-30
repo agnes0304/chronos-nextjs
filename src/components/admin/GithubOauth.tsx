c5fc6135c095130fc57a2a610dedcba3bf02ec87
agnes0304 <jiwoochoi0304@gmail.com>
2023-11-30 14:43:48 +0900

    oauth ongoing

--- src/components/admin/GithubOauth.tsx
+++ src/components/admin/GithubOauth.tsx
@@ -1,24 +1,15 @@
 "use client";
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { User } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnon = process.env.NEXT_PUBLIC_SUPABASE_ANON || "";
+import { useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
+import { supabase } from "@/components/admin/SupaClient";
+
+
 
 const GithubOauth = () => {
   const [user, setUser] = useState<User | null>(null);
-
-  const GithubOauthHandler = async () => {
-    try {
-      await fetch(`${baseUrl}/signin/github`);
-    } catch (error) {
-      console.log("There was a problem with the fetch operation:", error);
-    }
-  };
 
   useEffect(() => {
     window.addEventListener("hashchange", function () {
@@ -34,7 +25,7 @@ const GithubOauth = () => {
     setUser(user);
   }
 
-  const supabase = createClient(supabaseUrl, supabaseAnon);
+  
 
   async function signInWithGithub() {
     await supabase.auth.signInWithOAuth({
@@ -45,6 +36,7 @@ const GithubOauth = () => {
   async function signOut() {
     await supabase.auth.signOut();
   }
+
   if (user) {
    return (
       <div>
@@ -68,16 +60,9 @@ const GithubOauth = () => {
         onClick={() => signInWithGithub()}
       >
         <FontAwesomeIcon icon={faGithub} className="mr-2" />
-        Github로 로그인 - 프론트
+        Github로 로그인
       </button>
-      <button
-        className="text-white bg-gray-600 hover:bg-gray-700 active:bg-gray-800 h-[42px] w-[160px] p-2 border rounded-full text-md flex justify-center items-center group px-2 transition-all duration-200 ease-in-out"
-        type="button"
-        onClick={() => GithubOauthHandler()}
-      >
-        <FontAwesomeIcon icon={faGithub} className="mr-2" />
-        Github로 로그인 - 서버
-      </button>
+    
     </div>
   );
 };
