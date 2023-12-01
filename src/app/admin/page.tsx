c2a61956845b8f5bb8b70ec000ef61ff276455ed
agnes0304 <jiwoochoi0304@gmail.com>
2023-12-01 19:40:35 +0900

    ongoing

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -23,7 +23,6 @@ const AdminPage = () => {
     <>
       {isLogin ? (
         <div className="flex flex-col w-[90vw] justify-center items-center gap-4">
-          <h1 className="text-lg text-gray-600">{loginedUserData.email}</h1>
           <button
             className="bg-indigo-300 text-white hover:bg-indigo-400 active:bg-indigo-400 h-[42px] w-[120px] p-2 border rounded-full text-md flex justify-center items-center group px-2 transition-all duration-200 ease-in-out"
             type="button"
