9f9f837f7b425b78dd19925bf50858bd11a26e32
agnes0304 <jiwoochoi0304@gmail.com>
2023-12-06 14:23:51 +0900

    added metadata on main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,37 @@ import "./globals.css";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+const meta = {
+  title: "필기깎는화석 | Chronos",
+  description: "역사 연표자료 및 필기노트 검색 서비스",
+  url: "https://chronos.jiwoo.best/",
+  img: ['https://chronos.jiwoo.best/ver1.5_OccupationSummary.webp', 'https://chronos.jiwoo.best/fossilLogo.png'],
+};
+
 export const metadata: Metadata = {
-  title: "Chronos",
-  description: "Home of the Chronos project",
+  title: meta.title,
+  description: meta.description,  
+  openGraph: {
+    title: meta.title,
+    description: meta.description,
+    url: meta.url,
+    siteName: meta.title,
+    images: [
+      {
+        url: meta.img[0],
+        width: 800,
+        height: 600,
+      },
+      {
+        url: meta.img[1],
+        width: 600,
+        height: 1600,
+        alt: "fossilLogo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
